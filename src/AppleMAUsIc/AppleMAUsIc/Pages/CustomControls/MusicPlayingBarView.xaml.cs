03e5ab3e307930595e17129e994b442972360d2f
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-05-21 20:48:40 +0200

    [#2] Add custom view for the playing bar

---
+++ src/AppleMAUsIc/AppleMAUsIc/Pages/CustomControls/MusicPlayingBarView.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace AppleMAUsIc.Pages.CustomControls;
+
+public partial class MusicPlayingBarView : ContentView
+{
+    public static readonly BindableProperty TrackTitleProperty =
+        BindableProperty.Create(nameof(Title), typeof(string), typeof(MusicPlayingBarView), string.Empty);
+
+    public string Title
+    {
+        get => (string)GetValue(MusicPlayingBarView.TrackTitleProperty);
+        set => SetValue(MusicPlayingBarView.TrackTitleProperty, value);
+    }
+
+    public static readonly BindableProperty ImageNameProperty =
+        BindableProperty.Create(nameof(CoverImageName), typeof(string), typeof(MusicPlayingBarView), string.Empty);
+
+    public string CoverImageName
+    {
+        get => (string)GetValue(MusicPlayingBarView.ImageNameProperty);
+        set => SetValue(MusicPlayingBarView.ImageNameProperty, value);
+    }
+
+    public MusicPlayingBarView()
+    {
+        InitializeComponent();
+    }
+}
